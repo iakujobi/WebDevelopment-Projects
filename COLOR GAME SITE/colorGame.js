64ed8eae3330b4fa38892d404080dc7c58ba2f18
iakujobi <->
2020-08-13 21:55:43 -0400

    2nd commit

---
+++ COLOR GAME SITE/colorGame.js
@@ -0,0 +1,167 @@
+// This is called Module Design Pattern(a type of Javascript design pattern) - add a bunch of properties into objects
+
+// var game = {}
+
+// game.init = function () {
+//    setupModeButtons();
+//    setupSquares();
+//    reset();
+// }
+
+// game.init();
+
+var numSquares = 6;
+var colors = [];
+var pickedColor;
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+
+init();
+
+function init() {
+	setupModeButtons();
+	setupSquares();
+	reset();
+}
+
+function setupModeButtons() {
+	//mode buttons event listeners
+	for (var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function() {
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+
+			//Tenery operator. This one line does the work of the if else statement below
+			this.textContent === "Easy" ? numSquares = 3 : numSquares = 6;
+
+			// if (this.textContent == "Easy") {
+			//    numSquares = 3;
+			// } else {
+			//    numSquares = 6;
+			// }
+
+			reset();
+		});
+	}
+}
+
+function setupSquares() {
+	for (var i = 0; i < squares.length; i++) {
+		//add click listeners to squares
+		squares[i].addEventListener("click", function() {
+			//grab color of clicked square
+			var clickedColor = this.style.backgroundColor;
+			//compare color to pickedColor
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = "Correct!";
+				resetButton.textContent = "Play Again?";
+				changeColors(clickedColor);
+				h1.style.backgroundColor = clickedColor;
+			} else {
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "Try Again";
+			}
+		});
+	}
+}
+
+function reset() {
+	//generate all new colors
+	colors = generateRandomColors(numSquares);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked color
+	colorDisplay.textContent = pickedColor;
+	resetButton.textContent = "New Colors";
+	messageDisplay.textContent = "";
+	//change colors of squares
+	for (var i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.display = "block";
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+	h1.style.backgroundColor = "steelblue";
+}
+
+//While refactor (cleaning up the code), the init() function replaced both functions
+
+// easyBtn.addEventListener("click", function () {
+//    //for styling the hover effect
+//    hardBtn.classList.remove("selected");
+//    easyBtn.classList.add("selected");
+//    numSquares = 3;
+//    //generate 3 random colors and hide the other ones
+//    colors = generateRandomColors(numSquares);
+//    pickedColor = pickColor();
+//    colorDisplay.textContent = pickedColor;
+//    for (var i = 0; i < squares.length; i++){
+//       if (colors[i]) {
+//          squares[i].style.backgroundColor = colors[i];
+//       } else {
+//          squares[i].style.display = "none";
+//       }
+//    }
+// });
+
+// hardBtn.addEventListener("click", function () {
+//     //for styling the hover effect
+//    easyBtn.classList.remove("selected");
+//    hardBtn.classList.add("selected");
+//    numSquares = 6;
+//    //generate 6 random colors
+//    colors = generateRandomColors(numSquares);
+//    pickedColor = pickColor();
+//    colorDisplay.textContent = pickedColor;
+//    for (var i = 0; i < squares.length; i++) {
+//       squares[i].style.backgroundColor = colors[i];
+//       squares[i].style.display = "block";
+//    }
+// });
+
+resetButton.addEventListener("click", function() {
+	reset();
+});
+
+function changeColors(color) {
+	//loop through all squares
+	for (var i = 0; i < squares.length; i++) {
+		//change each color to match given color
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num) {
+	//make an array
+	var arr = [];
+	//add num random colors to array
+	for (var i = 0; i < num; i++) {
+		//get random color and push into arr
+		arr.push(randomColor());
+	}
+	//return that array
+	return arr;
+}
+
+function randomColor() {
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	//pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256);
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
